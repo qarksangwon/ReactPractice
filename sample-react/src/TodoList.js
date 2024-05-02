33de3a077291f8dc5ada2c355e4c23e3ebd66630
qarksangdol <0629psw@gmail.com>
2024-05-02 11:03:18 +0900

    제거 기능 구현

--- sample-react/src/TodoList.js
+++ sample-react/src/TodoList.js
@@ -29,7 +29,27 @@ const TodoList = () => {
     // 사용자가 onClick으로 새 데이터를 추가했으면 다시 input안의 값을 비워주는 역할
   };
 
-  const namesList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  // 삭제하는 기능도 만들어보자.
+  // filter를 이용해 id값을 받아서 names 배열의 id값과 id값이 같은 것만
+  // filter로 없앤다. 그 후 setNames로 names의 state를 바꿔 렌더링
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
+  // onDoubleClick 즉 doubleClick이 일어났을 때 해당 id를 리스트에서 없애는
+  // onRemove를 실행시키는 이벤트를 등록.
+  // 여기서 () => onRemove(name.id) 를 사용하는 이유는
+  // 이벤트 발생 시 새로운 익명 함수를 만들어 거기서 onRemove()를 호출하는거고,
+  // 바로 onRemove(name.id)사용 해당 함수를 등록할 때 onRemove(name.id)가 바로 실행됨.
+  // 이는 상수로 만든 함수이기 때문에, 매개변수를 전달할 때 ()를 사용함으로
+  // 해당 함수가 등록될 때 바로 실행되기 때문에 익명 함수로 만들어야 하는 것.
+  // 기존 다른 언어의 방식이나 function onRemove () {} 식으로 만든 함수라면 위처럼 사용 가능하다.
+  const namesList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
   // 이렇게 되면, onChange 즉 input안의 값이 변경될 때 마다 onChange함수가 불러지고,
   // onChange함수는 setInputText(e.target.value) 즉 이벤트가 발생한 타겟의 밸류,
   // 즉 input 안의 값으로 set이 계속 이뤄지는것!
