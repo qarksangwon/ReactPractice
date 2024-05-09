3a0948d56fd7f8ced6cdf0d6ee49f2c0e86fbe54
qarksangdol <0629psw@gmail.com>
2024-05-09 11:11:25 +0900

    firebase 이미지 업로드, 다운로드

--- axios-ex/src/App.js
+++ axios-ex/src/App.js
@@ -1,20 +1,10 @@
-import UserStore from "./context/UserStore";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
+import ImageUploader from "./pages/imgUpload";
 
 function App() {
   return (
-    <UserStore>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/Signup" element={<Signup />} />
-          <Route path="/Home" element={<Home />} />
-        </Routes>
-      </Router>
-    </UserStore>
+    <>
+      <ImageUploader />
+    </>
   );
 }
 
---
+++ axios-ex/src/imgUpload/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export const storage = firebase.storage();
